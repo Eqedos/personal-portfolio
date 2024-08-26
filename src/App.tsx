498922ac3fadf04dd00362075c8626f0be2c9544
Eqedos <parthkumarj117@gmail.com>
2024-08-27 00:58:24 +0900

    fix home

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,6 @@ import { useMediaQuery } from 'react-responsive';
 import { useHotkeys } from 'react-hotkeys-hook';
 import { Analytics } from '@vercel/analytics/react';
 
-
 const statusBarIcons = [
   { id: 'js', icon: DiJavascript1, color: "yellow" },
   { id: 'html', icon: DiHtml5, color: "blue" },
@@ -87,21 +86,21 @@ const App = () => {
     );
   }
   
-  
-
   return (
     <div className="flex flex-col h-screen overflow-hidden relative">
       <div className="absolute top-4 right-4 text-gray-400 text-xs opacity-50">
-  Press <kbd className="p-1 bg-gray-700">h</kbd> for help | Press <kbd className="p-1 bg-gray-700">t</kbd> for terminal
-</div>
-<main className="flex-grow p-4 relative z-10 flex justify-start items-start">
-      <div className="flex flex-col items-start z-30">
-      <div className="text-gray-200 text-xs opacity-50">Double click.</div>
-        <Icon text="About Me" onClick={toggleAboutMe} icon={FaUserAlt} />
-        <Icon text="Projects" onClick={toggleProjects} icon={FolderIcon} />
-        <Icon text="Contact" onClick={toggleContact} icon={FaPhoneAlt} />
-        <Icon text="Experience" onClick={toggleExperience} icon={FaBriefcase} />
+        Press <kbd className="p-1 bg-gray-700">h</kbd> for help | Press <kbd className="p-1 bg-gray-700">t</kbd> for terminal
       </div>
+      <main className="flex-grow p-4 relative z-10 flex justify-start items-start">
+        <div className="flex flex-col items-start z-30">
+          <div className="text-gray-200 text-sm opacity-90 font-semibold mb-2">
+            DOUBLE CLICK.
+          </div>
+          <Icon text="About Me" onClick={toggleAboutMe} icon={FaUserAlt} className="w-12 h-12" />
+          <Icon text="Projects" onClick={toggleProjects} icon={FolderIcon} className="w-12 h-12" />
+          <Icon text="Contact" onClick={toggleContact} icon={FaPhoneAlt} className="w-12 h-12" />
+          <Icon text="Experience" onClick={toggleExperience} icon={FaBriefcase} className="w-12 h-12" />
+        </div>
         <CenteredText
           mainText="Parth Kumar"
           subText="Student | Software Engineer"
@@ -114,7 +113,7 @@ const App = () => {
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
           >
-            <AboutMe onClose={toggleAboutMe}/>
+            <AboutMe onClose={toggleAboutMe} />
           </motion.div>
         )}
         {showContact && (
@@ -124,7 +123,7 @@ const App = () => {
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
           >
-            <Contact onClose={toggleContact}/>
+            <Contact onClose={toggleContact} />
           </motion.div>
         )}
         {showExperience && (
@@ -134,7 +133,7 @@ const App = () => {
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
           >
-            <Experience onClose={toggleExperience}/>
+            <Experience onClose={toggleExperience} />
           </motion.div>
         )}
         {showProjects && (
@@ -144,7 +143,7 @@ const App = () => {
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
           >
-            <Projects onClose={toggleProjects}/>
+            <Projects onClose={toggleProjects} />
           </motion.div>
         )}
         {showHelp && (
@@ -154,45 +153,45 @@ const App = () => {
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
           >
-            <Help onClose={toggleHelp}/>
+            <Help onClose={toggleHelp} />
           </motion.div>
         )}
       </main>
       {!showExperience && !showProjects && (
         <footer className="p-4 bg-black text-white flex items-center justify-center z-10">
           {statusBarIcons.map(iconData => (
-            <StatusBarElement key={iconData.id} onClick={() => {}} icon={iconData.icon} color={iconData.color} />
+            <StatusBarElement key={iconData.id} onClick={() => {}} icon={iconData.icon} color={iconData.color} className="w-10 h-10" />
           ))}
         </footer>
       )}
       {!showProjects && !showExperience && (
-  <div>
-    <div className="absolute left-0 bottom-0 p-4 flex items-center z-30">
-      <FaTerminal className="text-white w-6 h-6 cursor-pointer" onClick={toggleTerminal} />
-    </div>
-    <div className="absolute right-0 bottom-0 p-4 flex items-center z-30">
-      <motion.div
-        onClick={toggleColorPicker}
-        className="w-6 h-6 rounded-full bg-gradient-to-tr from-purple-400 via-pink-500 to-yellow-500 shadow cursor-pointer mr-2"
-        initial="hidden"
-        animate="visible"
-        variants={colorPickerVariants}
-        transition={{ type: "spring", stiffness: 300 }}
-      />
-      <Clock />
-    </div>
-    {showColorPicker && (
-      <ColorPickerModal
-        color={backgroundColor}
-        setColor={setBackgroundColor}
-        showModal={showColorPicker}
-        setShowModal={setShowColorPicker}
-      />
-    )}
-    {showTerminal && <Terminal onClose={toggleTerminal}/>}
-    <Analytics />
-  </div>
-)}
+        <div>
+          <div className="absolute left-0 bottom-0 p-4 flex items-center z-30">
+            <FaTerminal className="text-white w-8 h-8 cursor-pointer" onClick={toggleTerminal} />
+          </div>
+          <div className="absolute right-0 bottom-0 p-4 flex items-center z-30">
+            <motion.div
+              onClick={toggleColorPicker}
+              className="w-8 h-8 rounded-full bg-gradient-to-tr from-purple-400 via-pink-500 to-yellow-500 shadow cursor-pointer mr-2"
+              initial="hidden"
+              animate="visible"
+              variants={colorPickerVariants}
+              transition={{ type: "spring", stiffness: 300 }}
+            />
+            <Clock />
+          </div>
+          {showColorPicker && (
+            <ColorPickerModal
+              color={backgroundColor}
+              setColor={setBackgroundColor}
+              showModal={showColorPicker}
+              setShowModal={setShowColorPicker}
+            />
+          )}
+          {showTerminal && <Terminal onClose={toggleTerminal} />}
+          <Analytics />
+        </div>
+      )}
     </div>
   );
 }
